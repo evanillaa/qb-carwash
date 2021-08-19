c7ab8d478d9c5b47f53df77a473e285c8727d020
vanillaa <80897991+vanillla1@users.noreply.github.com>
2021-08-19 13:45:15 +0300

    fix

--- config.lua
+++ config.lua
@@ -3,27 +3,27 @@ Config = {}
 Config.Locations = {
     [1] = {
         ["label"] = "Hands Free Carwash",
-        vector3(26.5906, -1392.0261, 27.3634),
+        ["coords"] = vector3(26.5906, -1392.0261, 27.3634),
     },
     [2] = {
         ["label"] = "Hands Free Carwash",
-        vector3(167.1034, -1719.4704, 27.2916),
+        ["coords"] = vector3(167.1034, -1719.4704, 27.2916),
     },
     [3] = {
         ["label"] = "Hands Free Carwash",
-        vector3(-74.5693, 6427.8715, 29.4400),
+        ["coords"] = vector3(-74.5693, 6427.8715, 29.4400),
     },
     [4] = {
         ["label"] = "Hands Free Carwash",
-        vector3(-1200.4, -1720.46, 3.40),
+        ["coords"] = vector3(-1200.4, -1720.46, 3.40),
     },
     [5] = {
         ["label"] = "Hands Free Carwash",
-        vector3(1363.22, 3592.7, 34.41),
+        ["coords"] = vector3(1363.22, 3592.7, 34.41),
     },
     [6] = {
         ["label"] = "Hands Free Carwash",
-        vector3(-699.6325, -932.7043, 17.0139),
+        ["coords"] = vector3(-699.6325, -932.7043, 17.0139),
     }
 }
 
